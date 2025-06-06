6b0b1637a6352a997023f69596cd0adca72a927f
Kamila Silva <kamilaechelon2@gmail.com>
2025-06-06 15:03:05 -0300

    exercicio1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// Exercício1
+		// Exercício1, faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, 
+		// conforme exemplos.
 
 		Scanner sc = new Scanner(System.in);
 
@@ -11,9 +12,9 @@ public class Main {
 
 		x = sc.nextInt();
 		y = sc.nextInt();
-		
+
 		soma = y + x;
-		
+
 		System.out.println("SOMA = " + soma);
 
 		sc.close();
